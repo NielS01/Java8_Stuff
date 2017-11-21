0371a8e1fa5ff7642a421e858e2e7e94c285ec39
Niel <neil717@optusnet.com.au>
2017-11-21 21:52:51 +1100

    More and streams - Collectors

--- Java8_Tute/src/com/niels/tute/streams/menu/StreamExample.java
+++ Java8_Tute/src/com/niels/tute/streams/menu/StreamExample.java
@@ -2,6 +2,7 @@ package com.niels.tute.streams.menu;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class StreamExample {
@@ -21,12 +22,29 @@ public class StreamExample {
 		// 1. filter
 		// 2. map
 		// 3. limit
-		// 4. collect
+		// 4. collect- terminal operation
 		
 		// Print name of first 2 meat dishes
 		List <String> twoMeatDishes = menu.stream().filter(d -> d.getType().equals(Dish.Type.MEAT)).map(Dish::getName).limit(2).collect(Collectors.toList());
 		System.out.println(twoMeatDishes.toString());
 		List <String> allMeatDishes = menu.stream().filter(d -> d.getType().equals(Dish.Type.MEAT)).map(Dish::getName).collect(Collectors.toList());
 		System.out.println(allMeatDishes);
+		
+		// Use other collectors
+		
+		// 1 - joining
+		String colonSeparatedPdishes = menu.stream().filter(d->d.getName().startsWith("p")).
+				map(Dish::getName).collect(Collectors.joining(":"));
+		System.out.println(colonSeparatedPdishes); //pork:pizza:prawns
+		
+		// 2 - summingInt
+		int caloriesTotalFishDish = menu.stream().filter(d->d.getType().equals(Dish.Type.FISH)).collect(Collectors.summingInt(Dish::getCalories));
+		System.out.println(String.format("Calories total for fish dishes = [%d]", caloriesTotalFishDish));
+		
+		Map<Dish.Type, List<Dish>> byDishType = menu.stream().collect(Collectors.groupingBy(Dish::getType));
+		// Print by dish type
+		// see: https://www.mkyong.com/java8/java-8-foreach-examples/
+		byDishType.forEach((k,v)-> {System.out.println(k.name() +":" ); 
+							v.forEach(d -> System.out.println("\t"+d.getName())); });
 	}
 }
