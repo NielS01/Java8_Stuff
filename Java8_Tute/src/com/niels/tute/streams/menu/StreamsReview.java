502aa169b84b62f45ea5ff4f31bf33cc9f74cedb
Niel <neil717@optusnet.com.au>
2018-01-29 21:49:38 +1100

    final quizes review

--- Java8_Tute/src/com/niels/tute/streams/menu/StreamsReview.java
+++ Java8_Tute/src/com/niels/tute/streams/menu/StreamsReview.java
@@ -2,6 +2,7 @@ package com.niels.tute.streams.menu;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class StreamsReview {
@@ -47,11 +48,26 @@ public class StreamsReview {
 
 
 		// 5.  Are any traders based in Milan?
-
+		Boolean anyMilanTraders = StreamSources.transactions.stream()
+								.map(Transaction::getTrader).anyMatch(trader -> trader.getCity().equalsIgnoreCase("Milan"));
+		System.out.println("Are there traders in Milan? " +anyMilanTraders);
+		
 		// 6.  Print all transactions’ values from the traders living in Cambridge.
-
+		System.out.println("Print all transactions’ values from the traders living in Cambridge.");
+		StreamSources.transactions.stream()
+				.filter(a -> a.getTrader().getCity().equalsIgnoreCase("Cambridge"))
+				.distinct()
+				.forEach(trans -> System.out.println(trans.getValue()));
+		
 		// 7.  What’s the highest value of all the transactions?
-
+		Optional<Integer> max = StreamSources.transactions.stream().map(Transaction::getValue).reduce(Integer::max);
+		if (max.isPresent()) {
+			System.out.println("Maximum transaction value is: " +max.get());
+		}
 		// 8.  Find the transaction with the smallest value.
+		Optional<Integer> min = StreamSources.transactions.stream().map(Transaction::getValue).reduce(Integer::min);
+		if (min.isPresent()) {
+			System.out.println("Min transaction value is: " +min.get());
+		}
 	}
 }
